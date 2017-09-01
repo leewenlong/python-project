55c58c76dd7b685f890e679455a84eb2b873ba25
liwenlong <liwenlong@oneapm.com>
2017-09-01 19:39:21 +0800

    finished top250

--- douban/spiders/MovieTop250.py
+++ douban/spiders/MovieTop250.py
@@ -11,11 +11,17 @@ class Top250(scrapy.Spider):
         # print response.body
         for i in response.xpath("//ol/li"):
             item = DoubanItem()
-            item['title'] = i.xpath('.//div[@class="hd"]/a/span/text()').extract()[0].encode("utf-8")
-
+            item['title'] = i.xpath('.//div[@class="hd"]/a/span/text()').extract()[0]
+            date = i.xpath('.//div[@class="bd"]/p/text()').extract()[1]
+            item['date'] = filter(str.isdigit, date.encode("utf-8"))
             item['score'] = i.xpath('.//span[@class="rating_num"]/text()').extract()[0].encode("utf-8")
-            people  = i.xpath('.//div[@class="bd"]/div/span/text()').extract()[1].encode("utf-8")
+            people = i.xpath('.//div[@class="bd"]/div/span/text()').extract()[1].encode("utf-8")
             item['people'] = filter(str.isdigit, people)
+            yield item
 
-            print item
-            pass  #待完成
+        next_url = i.xpath('//div[@class="paginator"]/a/@href').extract()
+        print next_url
+        for n in next_url:
+            url = "https://movie.douban.com/top250" + n.encode("utf-8")
+            print url
+            yield scrapy.Request(url)
