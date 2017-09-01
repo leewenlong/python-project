05179fb456fe8ebb60e1cb9124ea030aee548a06
liwenlong <liwenlong@oneapm.com>
2017-09-01 17:51:17 +0800

    init

---
+++ douban/items.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class DoubanItem(scrapy.Item):
+    title = scrapy.Field()
+    score = scrapy.Field()
+    people = scrapy.Field()
+    date = scrapy.Field()
+
+
---
+++ start.py
@@ -0,0 +1,3 @@
+import scrapy.cmdline
+
+scrapy.cmdline.execute("scrapy crawl example".split())
